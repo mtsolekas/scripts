4f2f0b35f73d29d356aea059741a7f777805ed0e
Marios Tsolekas <marios.tsolekas@gmail.com>
2017-03-12 16:19:56 +0200

    Added arguement check

--- music_control.pl
+++ music_control.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+die("No arguements\n") if ($#ARGV < 0);
+
 unless (-f $ENV{HOME}."/.config/mpd/pid") {
     system("mpd && mpc -q clear && mpc -q update &&
             mpc -q add / && mpc -q random on &&
