365228a9fe2fe474684991c28a7a74c401ef9e08
Marios Tsolekas <marios.tsolekas@gmail.com>
2017-06-24 05:56:24 +0300

    Changed avconv code to use libopus

--- opus_download.pl
+++ opus_download.pl
@@ -10,8 +10,12 @@ die("No url given\n") unless (@ARGV);
 my $client = WWW::YouTube::Download->new();
 my $id = WWW::YouTube::Download->video_id($ARGV[0]);
 
+print("Downloading info\n");
+my $info = $client->prepare_download($id);
+
+print("Downloading $info->{title}\n");
 $client->download($id, {fmt => 43, filename => "temp.webm"});
 
-my $info = $client->prepare_download($id);
-system("avconv -i temp.webm -vn -acodec copy \"$info->{title}.opus\"");
+print("Converting to opus\n");
+system("avconv -i temp.webm -vn -acodec libopus \"$info->{title}.opus\"");
 unlink("temp.webm");
