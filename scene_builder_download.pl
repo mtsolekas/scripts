7bc60bcd456f5a1ad3d4f6ba1f8c709eac0f10ff
Marios Tsolekas <marios.tsolekas@gmail.com>
2017-09-01 01:16:35 +0300

    Added SceneBuilder downloader/updater

---
+++ scene_builder_download.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use LWP::UserAgent ();
+
+my $ua = LWP::UserAgent->new(timeout => 60,
+                             env_proxy => 1,
+                             show_progress => 1);
+
+my $response = $ua->get("http://gluonhq.com/download/scene-builder-jar/");
+die("Download failed\n") unless ($response->is_success());
+
+my $target = $ENV{HOME}."/Workspace/scene_builder.jar";
+unlink($target) if (-f $target);
+
+open(my $out, ">", $target);
+print ($out $response->content);
+close($out);
