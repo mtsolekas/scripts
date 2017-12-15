cc2eac13f41e5742362df17facaf46ed2a51c965
Marios Tsolekas <marios.tsolekas@gmail.com>
2017-12-15 14:31:27 +0200

    Created backup script

---
+++ data_backup.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use File::Copy::Recursive qw(rcopy pathrm);
+
+my $config_path = "$ENV{HOME}/.config/data_backup.conf";
+open my $in, "<", $config_path or die "Couldn't open $config_path\n";
+my $backup_path = <$in>; chomp $backup_path;
+my @files = <$in>; chomp @files;
+close $in;
+
+die "Backup path not found\n" unless -d $backup_path;
+
+my @time = localtime;
+my $day = $time[3];
+my $month = $time[4] + 1;
+my $year = $time[5] + 1900;
+
+$backup_path .= "/$year-$month-$day";
+print "Backup destination: $backup_path\n";
+
+for (@files) {
+    print "Copying $_ ... ";
+    rcopy($_, "$backup_path/$_") or print "FAIL\n";
+}
+
+print "Done\n";
