1b0caa663b2138bbf534ea2150972d4a67df3fbc
Marios Tsolekas <marios.tsolekas@gmail.com>
2018-09-22 12:31:19 +0300

    Edited some variable names to be more descriptive

--- repository_watch.pl
+++ repository_watch.pl
@@ -9,8 +9,8 @@ use Desktop::Notify;
 sub get_dates {
     my ($user, $repo, $ua, $notification) = @_;
 
-    my $res = $ua->get("https://github.com/$user/$repo/commits/master");
-    unless ($res->is_success) {
+    my $resp = $ua->get("https://github.com/$user/$repo/commits/master");
+    unless ($resp->is_success) {
         $notification->body("Unable to fetch page");
         $notification->show;
         exit;
@@ -19,33 +19,33 @@ sub get_dates {
     my %months = (Jan => 1, Feb => 2, Mar => 3, Apr => 4,
                   May => 5, Jun => 6, Jul => 7, Aug => 8,
                   Sep => 9, Oct => 10, Nov => 11, Dec => 12);
-    my ($date) = $res->content =~ /Commits on (.*)/; $date =~ s/,//;
+    my ($date) = $resp->content =~ /Commits on (.*)/; $date =~ s/,//;
     my @dates = split / /, $date; $dates[0] = $months{$dates[0]};
     return @dates;
 }
 
 my $config = "$ENV{HOME}/.config/repository_watch.conf";
 open my $in, "<", $config or die "Couldn't open $config\n";
-my @repopairs = <$in>;
+my @repos = <$in>;
 close $in;
 
 my $ua = LWP::UserAgent->new(timeout => 30, env_proxy => 1);
 my $notification = Desktop::Notify->new->create(summary => "Repository Watch",
                                                 timeout => -1);
 my $msg;
-while (each @repopairs) {
-    my ($uuser, $urepo, $ouser, $orepo) = split / /, $repopairs[$_];
-    $orepo =~ s/\n//;
+while (each @repos) {
+    my ($up_user, $up_repo, $or_user, $or_repo) = split / /, $repos[$_];
+    $or_repo =~ s/\n//;
 
-    my @udates = get_dates($uuser, $urepo, $ua, $notification);
-    my @odates = get_dates($ouser, $orepo, $ua, $notification);
+    my @up_dates = get_dates($up_user, $up_repo, $ua, $notification);
+    my @or_dates = get_dates($or_user, $or_repo, $ua, $notification);
 
-    my @diff = ($udates[2] - $odates[2],
-                $udates[0] - $odates[0],
-                $udates[1] - $odates[1]);
+    my @diff = ($up_dates[2] - $or_dates[2],
+                $up_dates[0] - $or_dates[0],
+                $up_dates[1] - $or_dates[1]);
 
     $msg .= "\n" if $_;
-    $msg .= "$orepo: ";
+    $msg .= "$or_repo: ";
     if ($diff[0] > 0
         || ($diff[0] == 0 && $diff[1] > 0)
         || ($diff[0] == 0 && $diff[1] == 0 && $diff[2] > 0)) {
