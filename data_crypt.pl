fdbe91f2a4b80ba640c8f9a848915c116db84f87
Marios Tsolekas <marios.tsolekas@gmail.com>
2018-10-05 18:38:26 +0300

    Changed temporary archive name in Data Crypt

--- data_crypt.pl
+++ data_crypt.pl
@@ -13,16 +13,16 @@ if ($ARGV[0] eq "-e") {
     die "Input file/directory must be specified\n" unless $ARGV[2];
     die "Input file/directory must exist\n" unless -e $ARGV[2];
 
-    system "tar czvf .tmp.tgz $ARGV[2] &&
-            gpg -o $ARGV[1] -c .tmp.tgz &&
-            rm -rf .tmp.tgz $ARGV[2]";
+    system "tar czvf .tmp_archive $ARGV[2] &&
+            gpg -o $ARGV[1] -c .tmp_archive &&
+            rm -rf .tmp_archive $ARGV[2]";
 } elsif ($ARGV[0] eq "-d") {
     die "Input file must be specified\n" unless $ARGV[1];
     die "Input file/directory must exist\n" unless $ARGV[1];
 
-    system "gpg -o .tmp.tgz -d $ARGV[1] &&
-            tar xzvf .tmp.tgz &&
-            rm -rf .tmp.tgz $ARGV[1]"
+    system "gpg -o .tmp_archive -d $ARGV[1] &&
+            tar xzvf .tmp_archive &&
+            rm -rf .tmp_archive $ARGV[1]"
 } else {
     die "Invalid flag\n$usage";
 }
